c5c7dc6e2f6248c07b93e0f2d0bd6bf55445d18a
loliprotector <kaworinakano@gmail.com>
2019-12-26 01:04:37 +0800

    Create some basic char and string extensions

---
+++ RegexFX/src/CharExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexFX.src
+{
+    public static class CharExtensions
+    {
+        private static readonly Regex DigitRegex = new Regex(@"^\d$", RegexOptions.Compiled);
+        private static readonly Regex ControlRegex = new Regex(@"^\p{C}", RegexOptions.Compiled);
+        public static bool IsDigit(this char c) => DigitRegex.IsMatch(c.ToString());
+        public static bool IsControl(this char c) => ControlRegex.IsMatch(c.ToString());
+    }
+}
---
+++ RegexFX/src/String/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegexFX.String
+{
+    public static class StringExtensions
+    {
+        public static string Substring(this string s, int startIndex, int length = 1)
+        {
+            if (startIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "StartIndex cannot be less than zero.");
+
+            if (startIndex > s.Length)
+                throw new ArgumentOutOfRangeException(nameof(startIndex),
+                    "StartIndex cannot be larger than length of string.");
+
+            if (length < 0)
+                throw new ArgumentOutOfRangeException(nameof(length), "Length cannot be less than zero.");
+
+            if (startIndex > s.Length - length)
+                throw new ArgumentOutOfRangeException(nameof(length),
+                    "Index and length must refer to a location within the string.");
+
+            if (length == 0)
+                return string.Empty;
+
+            if (startIndex == 0 && length == s.Length)
+                return s;
+
+            var skip = new string('.', startIndex);
+            var regex = new Regex($@"(?<={skip}).{{1, {length}}}");
+            return regex.Match(s).Value;
+        }
+    }
+}
