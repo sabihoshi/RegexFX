b58b65ba64ad5c95b494c14e0886dae1c85af0f2
hizamakura <kaworinakano@gmail.com>
2019-12-29 01:43:37 +0800

    Add some parsing for int
    Am not that great at making this optimized, but it's regex why does it matter

--- RegexFX/src/CharExtensions.cs
+++ RegexFX/src/CharExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace RegexFX.src
 {
@@ -9,7 +6,9 @@ namespace RegexFX.src
     {
         private static readonly Regex DigitRegex = new Regex(@"^\d$", RegexOptions.Compiled);
         private static readonly Regex ControlRegex = new Regex(@"^\p{C}", RegexOptions.Compiled);
+
         public static bool IsDigit(this char c) => DigitRegex.IsMatch(c.ToString());
+
         public static bool IsControl(this char c) => ControlRegex.IsMatch(c.ToString());
     }
 }
---
+++ RegexFX/src/Numerics/Parsing.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexFX.Numerics
+{
+    public class Parsing
+    {
+        public Regex NumberRegex { get; } = new Regex(
+            $@"^(?<{Token.Int}>[+-]?[0-9,]+)((?({Token.Int})(?<-{Token.Int}>(?<{Token.Decimal}>[.][0-9]*)?)|[.][0-9]+))(e(?<{Token.Exponent}>[+-]?[0-9]+))?",
+            RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+
+        private static Regex CreateRegex(int number) => new Regex($"^{number}$", RegexOptions.Compiled);
+
+        public bool RegexTryParseInt32(string input, out int output)
+        {
+            output = default;
+            if (NumberRegex.IsMatch(input))
+            {
+                output = int.Parse(input);
+                return true;
+            }
+            return false;
+        }
+
+        protected enum Token
+        {
+            Int,
+            Decimal,
+            Exponent
+        }
+    }
+}
